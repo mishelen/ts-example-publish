c2918532ed62d7a578d01e75ed76de3f232d8c29
Mikhail Hozhy <mikhail.hozhy@gmail.com>
2019-01-28 01:45:14 +0300

    increase coverage

--- src/__tests__/Greeter.test.ts
+++ src/__tests__/Greeter.test.ts
@@ -1,5 +1,10 @@
-import { Greeter } from '../index';
+import { Greeter, Greeting } from '../index';
 
 test('My Greeter', () => {
   expect(Greeter('Carl')).toBe('Hello Carl');
+});
+
+test('My Greeting', () => {
+  const inst = new Greeting('Carl')
+  expect(inst.greeter()).toBe('Hello Carl');
 });
